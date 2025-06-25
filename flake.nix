df7e3bf1ca3b308fe156abff8320cc5502a3d8b9
Onésime BERNARD <bernard.onesime@gmail.com>
2025-06-25 16:14:51 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Python project with uv template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {self, ...}:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlay = [];
+        };
+        caelestia-cli = pkgs.python3Packages.buildPythonPackage {
+          pname = "caelestia";
+          version = "1.0";
+          format = "pyproject";
+          src = ./.;
+          propagatedBuildInputs = with pkgs; [
+            python3Packages.pillow
+            python3Packages.materialyoucolor
+          ];
+          nativeBuildInputs = with pkgs; [
+            python3Packages.hatchling
+            python3Packages.hatch-vcs
+          ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [uv sass];
+          shellHook = ''
+            uv sync && source .venv/bin/activate
+          '';
+        };
+        packages = {
+          caelestia-cli = caelestia-cli;
+        };
+      });
+}
